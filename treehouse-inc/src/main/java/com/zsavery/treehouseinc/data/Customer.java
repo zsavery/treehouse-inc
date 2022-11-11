78868f64c57276f7680a0d5e35fb8959d7707a76
zsavery <zsavery998@gmail.com>
2022-11-11 09:54:29 -0600

    Add Customer class

---
+++ treehouse-inc/src/main/java/com/zsavery/treehouseinc/data/Customer.java
@@ -0,0 +1,91 @@
+package com.zsavery.treehouseinc.data;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customers")
+public class Customer {
+
+	@Id
+	@GeneratedValue
+	@Column(name="customer_id")
+	private long id;
+	
+	@Column(name="address")
+	private String address;
+	
+	@Column(name="first_name")
+	private String first_name;
+	
+	@Column(name="last_name")
+	private String last_name;
+	
+	@Column(name="dob")
+	private Date dob;
+	
+	@Column(name="email")
+	private String email;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", address=" + address + ", first_name=" + first_name + ", last_name=" + last_name
+				+ ", dob=" + dob + ", email=" + email + "]";
+	}
+	
+	
+
+}
