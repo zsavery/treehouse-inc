ae6c834298ac5fb0aa9b1df807b5f3fd10897080
zsavery <zsavery998@gmail.com>
2022-11-11 15:24:58 -0600

    Fixed H2 foreign key sections

--- treehouse-inc/target/classes/schema.sql
+++ treehouse-inc/target/classes/schema.sql
@@ -1,41 +1,5 @@
-/*
-Treehouse Inc has
-
-CUSTOMER
-	- customer_id
-	- address
-	- first_name
-	- last_name
-	- dob
-	- email
-
-
-INVOICE
-	- invoice_id
-	- order_date
-	- fulfillment_date
-	- price
-
-MATERIAL
-	- wood_id
-	- name
-	- color
-	- price
-
-Window
-	- window_id
-	- name
-	- price
-
-Treehouse_model
-	- model_id
-	- name
-	- price
-
-*/
-
 create table customers (
-	customer_id serial ,
+	customer_id serial  primary key,
 	address varchar(40) not null,
 	first_name varchar(20) not null,
 	last_name varchar(20) not null,
@@ -44,36 +8,36 @@ create table customers (
 );
 
 create table materials (
-	material_id serial,
+	material_id serial primary key,
 	name varchar(20) not null,
 	color varchar(20) default 'brown',
 	price double not null
 );
 
 create table window_frames (
-	window_id serial,
+	window_id serial primary key,
 	frame_name varchar(20) not null,
 	price double
 );
 
 create table treehouse_models (
-	model_id serial,
+	model_id serial primary key,
 	model_name varchar(20) not null,
-	window_number int default 0,
-	size_category int default 1,
+	window_number int default 1,
+	window_id bigint,
+	foreign key (window_id) references window_frames(window_id),
 	price double not null
 );
 
-
-
---create table invoices (
---	invoice_id serial,
---	order_date date,
---	fulfillment_date date,
---	foreign key (customer_id) references customers,
---	foreign key (material_id) references materials,
---	foreign key (window_id) references window_frames,
---	foreign key (model_id) references treehouse_models,
---	price double
---
---);
+create table invoices (
+	invoice_id serial primary key,
+	order_date date,
+	fulfillment_date date,
+	customer_id bigint,
+	foreign key (customer_id) references customers(customer_id),
+	material_id bigint,
+	foreign key (material_id) references materials(material_id),
+	model_id bigint,
+	foreign key (model_id) references treehouse_models(model_id),
+	price double
+);
